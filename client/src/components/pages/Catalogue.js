082d7ee9abaafc7f0448ad398a1b34ee87e45e42
Ji Wang <tomw0621@gmail.com>
2018-07-31 14:11:54 -0700

    all api calls and relevant functions converted to stately requests

--- client/src/components/pages/Catalogue.js
+++ client/src/components/pages/Catalogue.js
@@ -1,37 +1,41 @@
 import React from "react";
-import Axios from "axios";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 //this module is responsible for the display feed of all art uploaded to the site
-
-(Axios.get("/api/users/").then(function(response) {
-response.users.map(user => {
-    return(
-        
-        <div> <h3>Works by {user.userName}</h3>
-            {
-            user.posts.map(post =>{
-            return (
-                <div>
-                        <h3>{post.postName}</h3><br/>
-                        <img src={post.postLink}/>
-                        <button authorId={user._id} postId={post._id} onClick={this.likeToKnowMore}>More Info</button>
-                </div>
-            )
-            })      
-            }
-        
-        {function likeToKnowMore(a) {
-            a.preventDefault();
-            console.log("I would like to know more!");
-            // navigate to "/Item/{authorId}/{postId}" with axios.get(/api/{authorId}/{postId})
-            axios.get(`api/${this.authorId}/${this.postId}`).then(function(response){
-                // update state with response
-                // redirect to item.js
+class Catalogue extends React.Component {
+    state = {
+        results: []
+    }
+    componentDidMount() {
+        axios.get("/api/users/").then((response) => {
+            this.setState({
+                results: response.data
             })
-        }}
-        </div>
-    )     
-});
-}))
+        });
+    }
 
+    render() {
+        return (
+            this.state.results.map(user => {
+                <div>
+                    <h3>Works by {user.userName}</h3>
+                    {
+                        user.posts.map(post => {
+                            return (
+                                <div>
+                                    <h3>{post.postName}</h3><br />
+                                    <img src={post.postLink} />
+                                    <Link authorId={user._id} postId={post._id} to={`/item/${authorId}/${postId}`}>More Info</Link>
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+            })
+        )
+    }
+}
 export default Catalogue;
